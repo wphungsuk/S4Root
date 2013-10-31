5f5f67a3235a309cdcf6de62a5ec7d4d41a82060
Christian <chris.arevir@gmail.com>
2013-10-30 21:56:16 -0700

    Added checks for baseband and phone type to ensure that it won't be used on any other device (testing will be needed).
    Cleaned up code to be more readable
    
    Still need to copy files over, etc

--- S4Root/src/main/java/arevir/chris/s4root/MainActivity.java
+++ S4Root/src/main/java/arevir/chris/s4root/MainActivity.java
@@ -1,59 +1,53 @@
 package arevir.chris.s4root;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.AlertDialog;
-import android.app.Fragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.ContextThemeWrapper;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.os.Build;
 import android.widget.Toast;
-
 import java.io.IOException;
 
 // TODO: Make dialog popups black to match theme
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu){
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     public void onRootClick(View v){
+        Log.d("interface", "clicked root button");
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         alertDialogBuilder.setTitle("Are you sure?");
 
         alertDialogBuilder
                 .setMessage("There is no way of unrooting available at the moment")
                 .setCancelable(false)
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
+                .setNegativeButton("No", new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
                         // if this button is clicked, just close the dialog box and do nothing
                         dialog.cancel();
                     }
                 })
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        dialog.cancel();
                         // If this button is clicked, start rooting process
                         root();
-                        // Currently does nothing
-                        dialog.cancel();
                     }
                 });
 
@@ -65,10 +59,9 @@ public class MainActivity extends Activity {
     }
 
     public void onTestClick(View v){
+        Log.d("interface", "clicked root test button");
         /* Will check the device for proper root capabilities */
         boolean root;
-        String resultMessage = "";
-        String resultTitle = "";
         Process p;
 
         try{
@@ -79,18 +72,53 @@ public class MainActivity extends Activity {
         }
 
         if (root){
-            resultMessage += "Your device appears to be rooted!";
-            resultTitle += "Congrats!";
+            Log.d("interface", "successful root check");
+            simpleAlertMessage("Congrats!", "Your device appears to be rooted!");
         }
         else{
-            resultMessage += "Your device does not appear to be rooted, something must have gone wrong";
-            resultTitle += "Uh oh...";
+            Log.d("interface", "not a successful root check");
+            simpleAlertMessage("Uh oh...", "Your device does not appear to be rooted, something must have gone wrong");
         }
+    }
 
+    public void root(){
+        Log.d("rooting", "enter root process");
+        Log.d("rooting", "getting device name");
+        String device = getDeviceName();
+        Log.d("rooting", "getting baseband version");
+        String baseband = Build.getRadioVersion();
+
+        // Checking that this is a Samsung Galaxy S4 running the MF3 firmware
+        if(device.equals("SAMSUNG-SGH-I337") & baseband.equals("I337UCUAMF3")){
+            Log.d("rooting", "correct phone");
+            // TODO: Copy files over from assets to external SD
+            // Log.d("rooting", "attempting to copy files from assets to root of external SD");
+            // TODO: Execute pwn
+            // Log.d("rooting", "executing exploit");
+            // TODO: Execute script
+            // Log.d("rooting", "executing script");
+            // TODO: Clean up by deleting copied files
+            // Log.d("rooting", "executing cleanup procedure");
+        }
+        else{
+            Log.d("rooting", "incorrect phone");
+            simpleAlertMessage("Error", "This is not a Galaxy S4 running the MF3 Baseband, please look for a different way to root your phone!");
+        }
+    }
+
+    public String getDeviceName(){
+        return Build.MODEL;
+    }
+
+    public void toasty(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    public void simpleAlertMessage(String title, String message){
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setTitle(resultTitle);
+        alertDialogBuilder.setTitle(title);
         alertDialogBuilder
-                .setMessage(resultMessage)
+                .setMessage(message)
                 .setCancelable(false)
                 .setNegativeButton("Close", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
@@ -103,41 +131,5 @@ public class MainActivity extends Activity {
 
         // show it
         alertDialog.show();
-    }
-
-    public void root(){
-        // TODO: Check that this is a Samsung Galaxy S4 on MF3 firmware
-        //String firmware = "";
-        String deviceName= getDeviceName();
-        Toast.makeText(getApplicationContext(), deviceName, Toast.LENGTH_LONG).show();
-        // TODO: Check for exFat formatted external SD card
-        // TODO: Copy files over
-        // TODO: Execute pwn
-        // TODO: Execute script
-        // TODO: Clean up by deleting copied files
-    }
-
-    // The following two methods were adapted from http://stackoverflow.com/questions/1995439/get-android-phone-model-programmatically
-
-    public String getDeviceName() {
-        String manufacturer = Build.MANUFACTURER;
-        String model = Build.MODEL;
-        if (model.startsWith(manufacturer)) {
-            return capitalize(model);
-        } else {
-            return capitalize(manufacturer) + " " + model;
-        }
-    }
-
-    private String capitalize(String s) {
-        if (s == null || s.length() == 0) {
-            return "";
-        }
-        char first = s.charAt(0);
-        if (Character.isUpperCase(first)) {
-            return s;
-        } else {
-            return Character.toUpperCase(first) + s.substring(1);
-        }
     }
 }
